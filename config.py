f65a1d020a58963c64a8adf140a56be2c3388473
livecallclass <154664537+livecallclass@users.noreply.github.com>
2024-01-14 22:03:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,5 +11,5 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6305751172:AAG3f0lLXE-ivB1r3xP0VSvUfR0nKfURYfM")
     API_ID = int(os.environ["API_ID", 29759544]
     API_HASH = os.environ["API_HASH", "97030a871720998c6efc5debcce67aa4"]
-    AUTH_USERS = "6952370497"""
+    AUTH_USERS = "6027460020"""
 
